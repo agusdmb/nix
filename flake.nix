06e5a597c730971f3cf6a85e5877607737ad7bd7
Agustin Marquez <agusdmb@gmail.com>
2024-04-27 14:43:43 +0200

    change to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: 
