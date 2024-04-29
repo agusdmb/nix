5348f6d75a4a186f0945c3ad0d6c8008d9f5f0d2
Agustin Marquez <agusdmb@gmail.com>
2024-04-29 23:03:55 +0200

    move alacritty to home manager

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -107,8 +107,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    alacritty
-    unzip
     waybar
     mako
     libnotify
